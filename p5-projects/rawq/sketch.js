8325d4973e5b4243e423e2bec7431c25818635cd
beerus <mahirashab19@gmail.com>
2018-06-12 18:24:09 +0600

    new

--- p5-projects/rawq/sketch.js
+++ p5-projects/rawq/sketch.js
@@ -1,7 +1,23 @@
-function setup() {
+var url1 = "http://api.wordnik.com/v4/word.json/";
+var word = "rainbow";
+var url2 =
+  "/relatedWords?useCanonical=false&limitPerRelationshopType=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 
+var link;
+function setup() {
+  noCanvas();
+  link = createA("#", word);
+  link.mousePressed(askWordnik);
 }
 
-function draw() {
+function askWordnik() {
+  loadJSON(url1 + word + url2, gotData);
+}
 
+function gotData(data) {
+  console.log(data);
+  var index1 = floor(random(0, data.length));
+  var index2 = floor(random(0, data[index1].words.length));
+  word = data[index1].words[index2];
+  link.html(word);
 }
