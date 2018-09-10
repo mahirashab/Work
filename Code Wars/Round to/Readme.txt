93407fd0056d576198217b41c583ee1ac87a779c
Beerus <mahirashab18@gmail.com>
2018-09-10 09:55:28 -0700

    code wars

---
+++ Code Wars/Round to/Readme.txt
@@ -0,0 +1,8 @@
+This codes rounds a decimal number to a specified decimal palce.
+
+Use roundTo(decimalValue, ToPlace)
+
+where decimalValue is the number which is rounded to 
+toPlace point.
+
+Use the console.
---
+++ Code Wars/raw - Copy/main.js
@@ -0,0 +1,11 @@
+function add(first, secoend) {
+    if()
+    
+    for(let i = first.length - 1; i >= 0; i++) {
+        for (let j = secoend.length - 1; j >= 0; j++) {
+            let num1 = Number(first[i])
+            let num2 = Number(secoend[i])
+
+        }
+    }
+}
